aaeaa311a2de65dc18bf330fdcd4dfb500b5dbfe
Katherine <53545200+KatRootes@users.noreply.github.com>
2019-12-22 15:45:55 -0600

    Plotting but incorrectly.

--- assets/js/app.js
+++ assets/js/app.js
@@ -18,7 +18,7 @@ var height = svgHeight - margin.top - margin.bottom;
 // Create an SVG wrapper, append an SVG group that will hold our chart,
 // and shift the latter by left and top margins.
 var svg = d3
-  .select(".chart")
+  .select("#scatter")
   .append("svg")
   .attr("width", svgWidth)
   .attr("height", svgHeight);
@@ -28,8 +28,8 @@ var chartGroup = svg.append("g")
   .attr("transform", `translate(${margin.left}, ${margin.top})`);
 
 // Initial Params
-var chosenXAxis = "Poverty";
-var chosenYAxis = "Age";
+var chosenXAxis = "poverty";
+var chosenYAxis = "age";
 
 // function used for updating x-scale var upon click on axis label
 function xScale(researchData, chosenXAxis) {
@@ -79,6 +79,36 @@ function renderCircles(circlesGroup, newXScale, newYScale, chosenXaxis) {
     return circlesGroup;
   }
 
+ // function used for updating circles group with new tooltip
+function updateToolTip(chosenXAxis, circlesGroup) {
+
+    if (chosenXAxis === "age") {
+      var label = "Age:";
+    }
+    else {
+      var label = "Smokes:";
+    }
+  
+    var toolTip = d3.tip()
+      .attr("class", "tooltip")
+      .offset([80, -60])
+      .html(function(d) {
+        return (`${d.state}<br>${label} ${d[chosenXAxis]}`);
+      });
+  
+    circlesGroup.call(toolTip);
+  
+    circlesGroup.on("mouseover", function(data) {
+      toolTip.show(data);
+    })
+      // onmouseout event
+      .on("mouseout", function(data, index) {
+        toolTip.hide(data);
+      });
+  
+    return circlesGroup;
+  }
+
   // Retrieve data from the CSV file and execute everything below
 d3.csv("/assets/data/data.csv").then(function(researchData, err) {
     if (err) throw err;
@@ -120,39 +150,39 @@ d3.csv("/assets/data/data.csv").then(function(researchData, err) {
       .classed("x-axis", true)
       .attr("transform", `translate(0, ${height})`)
       .call(bottomAxis);
-  
+
     // append y axis
     chartGroup.append("g")
       .call(leftAxis);
   
     // append initial circles
-    // var circlesGroup = chartGroup.selectAll("circle")
-    //   .data(researchData)
-    //   .enter()
-    //   .append("circle")
-    //   .attr("cx", d => xLinearScale(d[chosenXAxis]))
-    //   .attr("cy", d => yLinearScale(d[chosenYAxis]))
-    //   .attr("r", 20)
-    //   .attr("fill", "blue")
-    //   .attr("opacity", ".5");
+    var circlesGroup = chartGroup.selectAll("circle")
+      .data(researchData)
+      .enter()
+      .append("circle")
+      .attr("cx", d => xLinearScale(d[chosenXAxis]))
+      .attr("cy", d => yLinearScale(d[chosenYAxis]))
+      .attr("r", 20)
+      .attr("fill", "blue")
+      .attr("opacity", ".5");
   
     //Create group for  2 x- axis labels
     var labelsGroup = chartGroup.append("g")
       .attr("transform", `translate(${width / 2}, ${height + 20})`);
   
-    var hairLengthLabel = labelsGroup.append("text")
+    var ageLabel = labelsGroup.append("text")
       .attr("x", 0)
       .attr("y", 20)
-      .attr("value", "hair_length") // value to grab for event listener
+      .attr("value", "age") // value to grab for event listener
       .classed("active", true)
-      .text("Hair Metal Ban Hair Length (inches)");
+      .text("Age");
   
-    var albumsLabel = labelsGroup.append("text")
+    var smokesLabel = labelsGroup.append("text")
       .attr("x", 0)
       .attr("y", 40)
-      .attr("value", "num_albums") // value to grab for event listener
+      .attr("value", "smokes") // value to grab for event listener
       .classed("inactive", true)
-      .text("# of Albums Released");
+      .text("smokes");
   
     // append y axis
     chartGroup.append("text")
@@ -161,55 +191,55 @@ d3.csv("/assets/data/data.csv").then(function(researchData, err) {
       .attr("x", 0 - (height / 2))
       .attr("dy", "1em")
       .classed("axis-text", true)
-      .text("Number of Billboard 500 Hits");
+      .text("TBD Label");
   
     // updateToolTip function above csv import
-    // var circlesGroup = updateToolTip(chosenXAxis, circlesGroup);
+    var circlesGroup = updateToolTip(chosenXAxis, circlesGroup);
   
-    // // x axis labels event listener
-    // labelsGroup.selectAll("text")
-    //   .on("click", function() {
-    //     // get value of selection
-    //     var value = d3.select(this).attr("value");
-    //     if (value !== chosenXAxis) {
+    //x axis labels event listener
+    labelsGroup.selectAll("text")
+      .on("click", function() {
+        // get value of selection
+        var value = d3.select(this).attr("value");
+        if (value !== chosenXAxis) {
   
-    //       // replaces chosenXAxis with value
-    //       chosenXAxis = value;
+          // replaces chosenXAxis with value
+          chosenXAxis = value;
   
-    //       // console.log(chosenXAxis)
+          // console.log(chosenXAxis)
   
-    //       // functions here found above csv import
-    //       // updates x scale for new data
-    //       xLinearScale = xScale(hairData, chosenXAxis);
+          // functions here found above csv import
+          // updates x scale for new data
+          xLinearScale = xScale(researchData, chosenXAxis);
   
-    //       // updates x axis with transition
-    //       xAxis = renderAxes(xLinearScale, xAxis);
+          // updates x axis with transition
+          xAxis = renderAxes(xLinearScale, xAxis);
   
-    //       // updates circles with new x values
-    //       circlesGroup = renderCircles(circlesGroup, xLinearScale, chosenXAxis);
+          // updates circles with new x values
+          circlesGroup = renderCircles(circlesGroup, xLinearScale, chosenXAxis);
   
-    //       // updates tooltips with new info
-    //       circlesGroup = updateToolTip(chosenXAxis, circlesGroup);
+          // updates tooltips with new info
+          circlesGroup = updateToolTip(chosenXAxis, circlesGroup);
   
-    //       // changes classes to change bold text
-    //       if (chosenXAxis === "num_albums") {
-    //         albumsLabel
-    //           .classed("active", true)
-    //           .classed("inactive", false);
-    //         hairLengthLabel
-    //           .classed("active", false)
-    //           .classed("inactive", true);
-    //       }
-    //       else {
-    //         albumsLabel
-    //           .classed("active", false)
-    //           .classed("inactive", true);
-    //         hairLengthLabel
-    //           .classed("active", true)
-    //           .classed("inactive", false);
-    //       }
-    //     }
-    //   });
+          // changes classes to change bold text
+          if (chosenXAxis === "age") {
+            ageLabel
+              .classed("active", true)
+              .classed("inactive", false);
+            smokesLabel
+              .classed("active", false)
+              .classed("inactive", true);
+          }
+          else {
+            smokesLabel
+              .classed("active", false)
+              .classed("inactive", true);
+            ageLabel
+              .classed("active", true)
+              .classed("inactive", false);
+          }
+        }
+      });
   }).catch(function(error) {
     console.log(error);
   });
